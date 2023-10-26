b1255e30a51eec1013fd04904b7990d0c59397a2
Ana Paula Souza Silva <78571318+PeuSouza@users.noreply.github.com>
2023-10-26 09:43:20 -0300

    front separado do back

---
+++ ServSimplesFront/src/main/java/ifpe/edu/br/ServSimplesFront/ServSimplesFrontApplication.java
@@ -0,0 +1,13 @@
+package ifpe.edu.br.ServSimplesFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServSimplesFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServSimplesFrontApplication.class, args);
+	}
+
+}
---
+++ ServSimplesFront/src/test/java/ifpe/edu/br/ServSimplesFront/ServSimplesFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpe.edu.br.ServSimplesFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServSimplesFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
